fce41d26b2697ab3c078236e49b2ba7b1dcab8f0
joyal <joyalthomas3052@gmail.com>
2024-04-27 00:27:34 +0100

    adedd setters

--- src/main/java/Utils/Task.java
+++ src/main/java/Utils/Task.java
@@ -21,14 +21,25 @@ public class Task {
     public String getOwner() {
         return owner;
     }
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 
     public String getDescription() {
         return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public LocalDate getDeadline() {
         return deadline;
     }
 
-
+    public void setDeadline(LocalDate deadline) {
+        if (deadline.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("Deadline cannot be in the past.");
+        }
+        this.deadline = deadline;
+    }
 }
