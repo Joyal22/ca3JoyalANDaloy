2d6928c31d95c135b3a313fc26d6815514fa1804
Aloysius <aloysiuspacheco2003@gmail.com>
2024-04-27 13:30:45 +0100

    Fixed bounded queue methods added new methods added javadocs

--- src/main/java/Utils/BoundedPriorityQueueSet.java
+++ src/main/java/Utils/BoundedPriorityQueueSet.java
@@ -80,6 +80,16 @@ public class BoundedPriorityQueueSet {
         }
         return this.queue.size();
     }
+
+    /**
+     * Inserts  a Task to the queue based on its priority. Checks
+     * if the queue is full and throws an IllegalStateException if it is
+     * if duplicate elements are found, throws a DuplicateElementException.
+     *
+     * @param task The Task to be added
+     * @throws IllegalStateException if the queue is full
+     * @throws DuplicateElementException if the Task is redundant
+     */
     public void add(Task task) throws DuplicateElementException {
         if (isFull()) {
             throw new IllegalStateException("Queue is full");
@@ -88,21 +98,73 @@ public class BoundedPriorityQueueSet {
         queue.add(position, task);
     }
 
-    public boolean offer(Task task) throws DuplicateElementException {
+    /**
+     * Inserts  a Task to the queue based on its priority.
+     * Checks if the queue is full or duplicate elems are found and returns false if it is
+     *
+     *
+     * @param task The Task to be added.
+     * @return true if the Task could be added, else false
+     */
+    public boolean offer(Task task) {
         if (isFull() || queue.contains(task)) {
             return false;
         }
-        int position = calcPosition(task);
-        queue.add(position, task);
-        return true;
+        try {
+            int position = calcPosition(task);
+            queue.add(position, task);
+            return true;
+        } catch (DuplicateElementException e) {
+            return false;
+        }
     }
 
-    public Task element() throws NoSuchElementException {
+    /**
+     * Gets a copy of the first Task in the queue
+     * throws no exception if the queue is empty
+     *
+     * @return The first Task in the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
+
+    public Task element() {
         if (isEmpty()) {
-            throw new NoSuchElementException("Queue is empty");
+            throw new NoSuchElementException("queue is empty");
         }
         return queue.getFirst();
     }
+    /**
+     * returns a copy of the first Task in the queue
+     * throws no exception
+     *
+     * @return The first Task in the queue, or null if the queue is empty
+     */
+    public Task peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        return queue.getFirst();
+    }
+
+    /**
+     * Deletes the first Task in the queue and returns it as a task object
+     * throws exception if the queue is empty
+     *
+     * @return The first Task in the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
+    public Task remove() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return queue.removeFirst();
+    }
+    /**
+     * Deletes the first Task in queue and returns it as a task object
+     * throws no exception if the queue is empty
+     *
+     * @return The first Task in the queue, or null if the queue is empty
+     */
 
     public Task poll() {
         if (isEmpty()) {
